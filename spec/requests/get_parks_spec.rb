12d1bdb166fd606fffea3ca29a97a4739ea8aecd
akane0915 <asialkane@gmail.com>
2017-07-21 16:48:37 -0700

    configure database_cleaner gem so rspec tests will pass

--- spec/requests/get_parks_spec.rb
+++ spec/requests/get_parks_spec.rb
@@ -1,7 +1,5 @@
 require 'rails_helper'
 
-DatabaseCleaner.start
-
 describe "get all parks route", type: :request do
 
   let!(:parks) { FactoryGirl.create_list(:park, 10)}
@@ -15,21 +13,27 @@ describe "get all parks route", type: :request do
   it 'returns status code 200' do
     expect(response).to have_http_status(200)
   end
+
+
+  it 'returns all parks by name' do
+    get '/parks?name=yellow'
+    output = JSON.parse(response.body).first
+    expect(output.fetch("name")).to eq 'Yellow Stone'
+  end
+
+  it 'returns all parks by year' do
+    get '/parks?year=1800'
+    output = JSON.parse(response.body).first
+    expect(output.fetch("year")).to eq 1800
+  end
 end
 
 
 describe "get specific park route", type: :request do
-
-  park = FactoryGirl.create(:park)
-
-  before { get "/parks/#{park['id']}"}
-
   it 'includes the park name in the response' do
+    park = FactoryGirl.create(:park)
+    get "/parks/#{park['id']}"
     expect(response.body).to include("Yellow Stone")
-  end
-
-  it 'returns status code 200' do
-    expect(response).to have_http_status(200)
   end
 end
 
