4f433a0145e9351434410400093e829b65cbf1ff
akane0915 <asialkane@gmail.com>
2017-07-21 11:51:11 -0700

    instantiate database clearner gem

--- spec/requests/get_parks_spec.rb
+++ spec/requests/get_parks_spec.rb
@@ -1,5 +1,7 @@
 require 'rails_helper'
 
+DatabaseCleaner.start
+
 describe "get all parks route", type: :request do
   let!(:parks) { FactoryGirl.create_list(:park, 10)}
 
@@ -7,6 +9,22 @@ describe "get all parks route", type: :request do
 
   it 'returns all parks' do
     expect(JSON.parse(response.body).size).to eq(10)
+  end
+
+  it 'returns status code 200' do
+    expect(response).to have_http_status(200)
+  end
+end
+
+DatabaseCleaner.clean
+
+describe "get specific park route", type: :request do
+  let!(:parks) { FactoryGirl.create(:park)}
+
+  before { get '/parks/1'}
+
+  it 'returns park with id 1' do
+    expect(JSON.parse(response.body).size).to eq(1)
   end
 
   it 'returns status code 200' do
